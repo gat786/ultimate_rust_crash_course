3374963ac8ca695bb80bb065a176f84befd0355d
Ganesh Tiwari <ganesht049@gmail.com>
2020-07-21 06:40:48 +0000

    done variables exercise

---
+++ exercise/a-variables/variables/src/main.rs
@@ -0,0 +1,12 @@
+const STARTING_MISSILES : i32= 8;
+const READY : i32= 2;
+
+fn main() {
+  let (mut missiles, ready)  = (STARTING_MISSILES, READY);
+
+  println!("Firing {} of {} missiles",ready,missiles);
+
+  // missiles = missiles - ready;
+
+  println!("{} missiles left",missiles - ready)
+}
